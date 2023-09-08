9000bae04146d5eeb2f7b231e5570b1a2bf366c9
Markus Graf <info@markusgraf.ch>
2023-09-08 14:11:14 +0200

    Start existing mongodb or run it newly

--- start_mongodb.sh
+++ start_mongodb.sh
@@ -1,3 +1,11 @@
 docker pull mongo
-#docker run --name mongodb -p 27017:27017 -d mongo
-docker run --name mongodb -p 27017:27017 mongo
+#!/bin/bash
+
+# Check if a container named "mongodb" exists
+if docker ps -a | grep -q mongodb; then
+    echo "Starting existing mongodb container..."
+    docker start mongodb
+else
+    echo "Running a new mongodb container..."
+    docker run --name mongodb -p 27017:27017 mongo
+fi
